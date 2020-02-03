bce6b904dcdf59ef04b9ef4a2d60537edbe723f6
weip <wdzaxiaowei@126.com>
2020-02-03 13:43:46 +0800

    android O 的 NotificationChannl 适配和 9.0 的权限适配

--- app/src/main/java/com/xiaowei/minemusic/service/MusicService.java
+++ app/src/main/java/com/xiaowei/minemusic/service/MusicService.java
@@ -1,12 +1,19 @@
 package com.xiaowei.minemusic.service;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+
+import androidx.annotation.RequiresApi;
 
 import com.xiaowei.minemusic.R;
 import com.xiaowei.minemusic.activitys.WelcomeActivity;
@@ -108,16 +115,50 @@ public class MusicService extends Service {
         /**
          * 创建Notification
          */
-        Notification notification = new Notification.Builder(this)
-                .setContentTitle(mMusicModel.getName())
-                .setContentText(mMusicModel.getAuthor())
-                .setSmallIcon(R.mipmap.logo_xb)
-                .setContentIntent(pendingIntent)
-                .build();
+        Notification notification = null;
+        /**
+         * android API 26 以上 NotificationChannel 特性适配
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = createNotificationChannel();
+            notification = new Notification.Builder(this, channel.getId())
+                    .setContentTitle(mMusicModel.getName())
+                    .setContentText(mMusicModel.getAuthor())
+                    .setSmallIcon(R.mipmap.logo)
+                    .setContentIntent(pendingIntent)
+                    .build();
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        } else {
+            notification = new Notification.Builder(this)
+                    .setContentTitle(mMusicModel.getName())
+                    .setContentText(mMusicModel.getAuthor())
+                    .setSmallIcon(R.mipmap.logo)
+                    .setContentIntent(pendingIntent)
+                    .build();
+        }
 
         /**
          * 设置 notification 在前台展示
           */
         startForeground(NOTIFICATION_ID, notification);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private NotificationChannel createNotificationChannel () {
+        String channelId = "imooc";
+        String channelName = "ImoocMusicService";
+        String Description = "ImoocMusic";
+        NotificationChannel channel = new NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_HIGH);
+        channel.setDescription(Description);
+        channel.enableLights(true);
+        channel.setLightColor(Color.RED);
+        channel.enableVibration(true);
+        channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        channel.setShowBadge(false);
+
+        return channel;
+
     }
 }
